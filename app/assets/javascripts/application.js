9561db479202e2cc01ec74736b084a3b7222e27a
Sherin Siew <sz.sherin@gmail.com>
2014-03-02 01:33:28 +0800

    More styling. Full screen hike map created. Links functional.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,15 +15,3 @@
 //= require turbolinks
 //= require aqicn_kimono_scripts
 //= require_tree .
-
-
-window.onload = function() {
-    var myOptions = {
-        center: new google.maps.LatLng(40.744556,-73.987378),
-        zoom: 18,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableDefaultUI: true
-    };
-
-    var map = new google.maps.Map(document.getElementById("gmap"), myOptions);
-}
